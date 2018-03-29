e23d79eb3007f9997c9d565edb4154084cc0ba29
Laura Messa Travin <laura.travin@gmail.com>
2018-03-29 21:45:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,25 +1,26 @@
 
 # THIS WORKS
-def oxford_comma(array)
-  if array.length == 1
-
-     array.join
-  elsif  array.length == 2
-
-     array.join(" and ")
-  elsif
-       array[-1] = "and " + array.last
-       array.join(", ") 
-  end
-end
-
-# BUT THIS NOT.  THE ARRAY LEGNTH IS 1 BUT IF LINE HAS NO EFFECT.
 # def oxford_comma(array)
 #   if array.length == 1
 #
 #      array.join
-#   if  array.length == 2
+#   elsif  array.length == 2
 #
 #      array.join(" and ")
+#   elsif
+#        array[-1] = "and " + array.last
+#        array.join(", ")
 #   end
 # end
+
+# BUT THIS NOT.  THE ARRAY LEGNTH IS 1 BUT IF LINE HAS NO EFFECT.
+def oxford_comma(array)
+  if array.length == 1
+
+     array.join
+    end
+  if  array.length == 2
+
+     array.join(" and ")
+  end
+end
